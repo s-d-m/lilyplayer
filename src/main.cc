4ec47c999397f823af0f2c0ebd00f91df0394aa3
Samuel DA MOTA <da.mota.sam@gmail.com>
2015-12-21 01:02:18 +0100

    Integrated a dark theme for the windows
    It now has a dark background

--- src/main.cc
+++ src/main.cc
@@ -2,11 +2,16 @@
 #include "signals_handler.hh"
 #include "mainwindow.hh"
 
+static const char* stylesheet =
+  #include "../qdarkstyle/style.qss"
+  ;
+
 int main(int argc, char** argv)
 {
   set_signal_handler();
 
   QApplication a(argc, argv);
+  a.setStyleSheet(stylesheet);
   MainWindow w;
   w.show();
 
