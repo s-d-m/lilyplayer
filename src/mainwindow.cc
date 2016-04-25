s-d-m/lilyplayer

commit 3591270fe843c59e5683a4e59db7dfc5d7e99615

slight factorisation, + wait 3 seconds at end of song

src/mainwindow.cc
@@ -136,31 +136,31 @@ void MainWindow::process_music_sheet_event(const music_sheet_event& event)
 
 void MainWindow::song_event_loop()
 {
-  if (is_in_pause.load())
+  if (is_in_pause.load() or (song_pos == INVALID_SONG_POS))
   {
     QTimer::singleShot(100, this, SLOT(song_event_loop()));
     return;
   }
 
-  if (song_pos == INVALID_SONG_POS)
+  const auto nb_events = this->song.events.size();
+  if (song_pos == nb_events)
   {
+    stop_song();
     QTimer::singleShot(100, this, SLOT(song_event_loop()));
-    return;
   }
 
-
-  if ((song_pos != 0) and (song_pos >= this->song.events.size()))
+  if (song_pos > nb_events)
   {
     throw std::runtime_error("Invalid song position found");
   }
 
   process_music_sheet_event( song.events[song_pos] );
 
-  if (song_pos + 1 == this->song.events.size())
+  if (song_pos + 1 == nb_events)
   {
-    // song is finished
-    stop_song();
-    QTimer::singleShot(100, this, SLOT(song_event_loop()));
+    // wait 3 seconds so that user has time to see the last keys up
+    // before the music sheet disappear
+    QTimer::singleShot(3000, this, SLOT(song_event_loop()));
   }
   else
   {

commit 5d3c84cdce3edfac33af7c827bd3093ebca3594f

Bugfix: don't process anthing more after stoping song.
This bug was introduced in commit 3591270fe843c59e5683a4e59db7dfc5d7e99615

The consequence here is that after processing the last event, it will
call stop_song, which would reset the song_pos to INVALID_SONG_POS.
Since the return was missing, the code would continue to execute, and
then immediately after, it would check for song_pos > nb_events.
This would now pass, leading to the execpetion.

src/mainwindow.cc
@@ -147,6 +147,7 @@ void MainWindow::song_event_loop()
   {
     stop_song();
     QTimer::singleShot(100, this, SLOT(song_event_loop()));
+    return;
   }
 
   if (song_pos > nb_events)